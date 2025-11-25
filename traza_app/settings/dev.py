77674c33937f8b38502cd4641a76ca42fe614f51
Alexspinosa <alexspinosar@gmail.com>
2025-11-24 22:23:18 -0500

    Proyecto inicial con django/ wagtail y estructura base

---
+++ traza_app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#5fk&&_a&=608o6%si079#kn7+s=4efgqn951b&mi*fg=958hf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
